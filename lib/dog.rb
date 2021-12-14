6e199986581e2506de41fc6ce047282ab24d1b27
Bennett <bjupfield@gmail.com>
2021-12-14 12:34:08 -0600

    commit tree 7e42ed3186d9a5cc5172309bbaff39428219bbe2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-
+    attr_accessor :name
+    def initialize(name = "Nameless", breed = "Breedless")
+        @name = name
+        @breed = breed
+    end
+    def breed=(newbreed)
+        @breed = newbreed
+    end
+    def breed
+        @breed    
+    end
 end
